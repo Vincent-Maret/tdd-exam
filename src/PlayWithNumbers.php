46c30f9e32ca6fefb7f0e68f031ba3559180bd41
Sir Papaya <maretvincent@protonmail.com>
2021-09-22 15:22:07 +0200

    feat: decimalToRoman throw error if n is negative

--- src/PlayWithNumbers.php
+++ src/PlayWithNumbers.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 final class PlayWithNumbers
 {
     public static function decimalToRoman(int $n): string {
-
+        if ($n < 0) {
+            throw new Exception('Unexpected: n should be greater than 0');
+        }
     }
 }
