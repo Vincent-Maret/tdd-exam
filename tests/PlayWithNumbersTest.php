4aa487437e2de8ec6ab8cff8ef12ea5530ac73e8
Sir Papaya <maretvincent@protonmail.com>
2021-09-22 15:21:39 +0200

    test: add test for decimalToRoman with negative value

--- tests/PlayWithNumbersTest.php
+++ tests/PlayWithNumbersTest.php
@@ -6,7 +6,11 @@ use PHPUnit\Framework\TestCase;
 
 final class PlayWithNumbersTest extends TestCase
 {
-    public function testDecimalToRoman(): void {
-        
+    /**
+     * Tests for decimalToRoman
+     */
+    public function testNegativeValue(): void {
+        $this->expectException(Exception::class);
+        PlayWithNumbers::decimalToRoman(-5);
     }
 }
