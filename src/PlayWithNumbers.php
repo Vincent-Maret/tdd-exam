29eacb8ad58c7059b717242e9a944ba00528fe57
Sir Papaya <maretvincent@protonmail.com>
2021-09-22 15:25:02 +0200

    feat: decimalToRoman throw error if n over 3000

--- src/PlayWithNumbers.php
+++ src/PlayWithNumbers.php
@@ -7,6 +7,8 @@ final class PlayWithNumbers
     public static function decimalToRoman(int $n): string {
         if ($n < 0) {
             throw new Exception('Unexpected: n should be greater than 0');
+        } else if ($n > 3000) {
+            throw new Exception('Unexpected: n should be lower or equal to 3000');
         }
     }
 }
